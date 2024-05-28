c6607d95fd5bf137f05dd7fe22a6e72ad27210d2
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-05-28 02:32:12 -0700

    styling Our Partners section

--- src/Components/Homepage/Partners/Partners.jsx
+++ src/Components/Homepage/Partners/Partners.jsx
@@ -1,7 +1,16 @@
+import KeelWorks from "../../../assets/images/KeelWorks-Logo.png";
+
 const Partners = () => {
   return (
-    <div className="bg-orange-200 flex items-center justify-center pt-10 pb-10 text-8xl font-bold">
-      Partners
+    <div className="bg-grey200 flex flex-col items-center justify-center pt-10 pb-10">
+      <h2 className="text-fontPrimary font-bold text-[3.56rem] leading-[85px]">
+        Our partners
+      </h2>
+      <div className="flex items-center justify-center gap-[15rem] mt-10">
+        <img src={KeelWorks} alt="" className="w-[8rem]" />
+        <img src={KeelWorks} alt="" className="w-[8rem]" />
+        <img src={KeelWorks} alt="" className="w-[8rem]" />
+      </div>
     </div>
   );
 };
