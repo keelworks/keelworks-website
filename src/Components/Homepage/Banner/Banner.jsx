37d96ab9d0c3d7df6fa8f1aaace005cb498bbe44
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-05-30 02:27:25 -0700

    making banner & partners responsive

--- src/Components/Homepage/Banner/Banner.jsx
+++ src/Components/Homepage/Banner/Banner.jsx
@@ -3,16 +3,18 @@ import slide1 from "../../../assets/images/banner.jpeg";
 const Banner = () => {
   return (
     <div
-      className="relative w-screen h-[calc(100vh-4rem)] bg-left bg-no-repeat bg-[size:150%]"
+      className="relative w-screen h-[calc(100vh-4rem)] overflow-hidden bg-center bg-no-repeat bg-cover lg:bg-[size:150%] lg:bg-left"
+      // className="relative w-screen h-[calc(100vh-4rem)] bg-left bg-no-repeat bg-[size:150%]"
       style={{
         backgroundImage: `url(${slide1})`,
         // backgroundSize: "cover",
         // backgroundPosition: "center",
       }}
     >
-      <div className="absolute top-0 left-0 w-1/2 h-full flex justify-center items-center bg-black bg-opacity-50 backdrop-blur-lg">
-        <div className=" mx-[3rem]">
-          <h1 className="text-[4rem] leading-[4.5rem] font-bold text-white">
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 backdrop-blur-[2px] md:w-1/2 lg:justify-start lg:backdrop-blur-lg">
+        {/* <div className="absolute top-0 left-0 w-1/2 h-full flex justify-center items-center bg-black bg-opacity-50 backdrop-blur-lg"> */}
+        <div className="mx-[2rem] md:mx-[8rem]">
+          <h1 className="text-[3rem] leading-[3.5rem] md:text-[4rem] md:leading-[4.5rem] font-bold text-white">
             We Can Do <br /> This! <br />
             But Only
             <br /> With
@@ -26,11 +28,12 @@ const Banner = () => {
             When we come together,
             <br /> we can transform lives.
           </h5>
-          <div className="flex gap-6 mt-6">
+          <div className="flex justify-between mt-6 lg:justify-normal lg:gap-6">
+            {/* <div className="flex gap-6 mt-6"> */}
             <button className="w-[116px] h-[47px] bg-primary500 rounded-full hover:bg-primary300 transition duration-[100ms] ease-linear text-[#101828] font-semibold">
               Donate
             </button>
-            <button className="w-[156px] h-[47px] border-primary500 border-solid border-[2px] rounded-full hover:bg-primary300 transition duration-[100ms] ease-linear text-primary500 hover:text-fontSecondary font-semibold">
+            <button className="w-[156px] h-[47px] border-primary500 border-solid border-[4px] rounded-full hover:bg-primary300 transition duration-[100ms] ease-linear text-primary500 hover:text-fontSecondary font-semibold">
               Get involved
             </button>
           </div>
--- src/Components/Homepage/Partners/Partners.jsx
+++ src/Components/Homepage/Partners/Partners.jsx
@@ -4,14 +4,27 @@ import teamgantt_logo from "../../../assets/images/Teamgantt-Logo-BW.png";
 
 const Partners = () => {
   return (
-    <div className="bg-grey200 flex flex-col items-center justify-center pt-10 pb-10">
-      <h2 className="text-fontPrimary font-bold text-[3.56rem] leading-[85px]">
+    <div className="w-full bg-grey200 flex flex-col items-center justify-center pt-10 pb-10">
+      <h2 className="text-fontPrimary font-bold text-[2rem] md:text-[3.56rem] leading-[85px]">
         Our partners
       </h2>
-      <div className="flex items-center justify-center gap-[15rem] mt-10">
-        <img src={adobe_logo} alt="" className="w-[16rem]" />
-        <img src={microsoft_logo} alt="" className="w-[16rem]" />
-        <img src={teamgantt_logo} alt="" className="w-[16rem]" />
+      <div className="w-[calc(100%-8rem)] flex items-center justify-between mt-10 lg:w-[calc(100%-16rem)]">
+        {/* <div className="flex items-center justify-center md:gap-[15rem] mt-10"> */}
+        <img
+          src={adobe_logo}
+          alt=""
+          className="w-[6rem] md:w-[10rem] lg:w-[16rem]"
+        />
+        <img
+          src={microsoft_logo}
+          alt=""
+          className="w-[6rem] md:w-[10rem] lg:w-[16rem]"
+        />
+        <img
+          src={teamgantt_logo}
+          alt=""
+          className="w-[6rem] md:w-[10rem] lg:w-[16rem]"
+        />
       </div>
     </div>
   );
