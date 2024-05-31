58f56a1945a4c218b3d27f5145c87bf3f7e21dce
HeenaRecordApp <heena.dhanani@keelworks.org>
2024-05-31 10:04:10 -0500

    image change

--- src/Components/Homepage/Cards/Cards.jsx
+++ src/Components/Homepage/Cards/Cards.jsx
@@ -1,7 +1,7 @@
 
 import slider1 from '../../../assets/images/card1.jpeg'
-import slider2 from '../../../assets/images/card2.jpeg'
-import slider3 from '../../../assets/images/card3.png'
+import slider2 from '../../../assets/images/card2.png'
+import slider3 from '../../../assets/images/card-3.png'
 import greaterImg from '../../../assets/images/_.png'
 const Cards = () => {
   return (
@@ -17,12 +17,12 @@ const Cards = () => {
       </div>
       <div className='flex flex-col bg-white w-full lg:h-screen '>
         <div className='justify-center flex p-10 '>
-          <p className='text-lg sm:text-3xl md:text-4xl lg:text-5xl font-extrabold '>The <span className='text-[#FBBC05] '>KeelWorks </span>Solutions</p>
+          <p className='text-lg sm:text-3xl md:text-4xl lg:text-6xl font-extrabold '>The <span className='text-[#FBBC05] '>KeelWorks </span>Solutions</p>
         </div>
         <div className='w-full grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  sm:gap-2 gap-6 lg:h-4/5'>
           <div className='overflow-hidden sm:m-2 md:m-4 lg:m-4 relative group  m-2 '>
             <img src={slider1} alt="" className='w-full h-full object-cover' />
-            <div className='intro h-1/6 w-full sm:p-4 md:p-2 lg:p-4 text-white absolute bottom-0 group-hover:h-4/5 group-hover:bottom-0 transition-all duration-500  group-hover:cursor-pointer  backdrop-blur-lg bg-black bg-opacity-60' >
+            <div className='intro h-1/6 w-full sm:p-4 md:p-2 lg:p-2 text-white absolute bottom-0 group-hover:h-4/5 group-hover:bottom-0 transition-all duration-500  group-hover:cursor-pointer  backdrop-blur-lg bg-black bg-opacity-60' >
               <p className='flex justify-between ml-2 items-center xl:justify-start lg:mt-8 md:mt-2 xl:mt-2 mt-8 w-full'>
                 {/* <span className='text-[#FBBC05] font-bold text-6xl sm:text-8xl md:text-5xl lg:text-7xl xl:text-9xl'>01</span> */}
                 <div className='w-4/5'>
@@ -41,7 +41,7 @@ const Cards = () => {
           </div>
           <div className='overflow-hidden  sm:m-2 md:m-4 lg:m-4   relative group m-2'>
             <img src={slider2} alt="" className='w-full h-full object-cover' />
-            <div className=' h-1/6 w-full sm:p-4 md:p-2 lg:p-4  text-white absolute bottom-0 group-hover:h-4/5  group-hover:bottom-0 transition-all duration-500 group-hover:cursor-pointer backdrop-blur-lg bg-black bg-opacity-60' >
+            <div className=' h-1/6 w-full sm:p-4 md:p-2 lg:p-2  text-white absolute bottom-0 group-hover:h-4/5  group-hover:bottom-0 transition-all duration-500 group-hover:cursor-pointer backdrop-blur-lg bg-black bg-opacity-60' >
               <p className='flex  justify-between items-center ml-4 lg:mt-8 md:mt-2 xl:mt-2 mt-8'>
                 <div className='w-4/5'>
                 <h1 className=' text-[#FBBC05] font-bold text-2xl sm:text-4xl md:text-4xl lg:text-3xl xl:text-6xl ml-4'>KeelMate</h1>
@@ -60,7 +60,7 @@ const Cards = () => {
           </div>
           <div className='overflow-hidden  sm:m-2 md:m-4 lg:m-4   relative group m-4'>
             <img src={slider3} alt="" className='w-full h-full object-cover' />
-            <div className=' h-1/6 w-full sm:p-4 md:p-2 lg:p-4 text-white absolute bottom-0 group-hover:h-4/5 group-hover:bottom-0 transition-all duration-500 group-hover:cursor-pointer backdrop-blur-lg bg-black bg-opacity-60' >
+            <div className=' h-1/6 w-full sm:p-4 md:p-2 lg:p-2 text-white absolute bottom-0 group-hover:h-4/5 group-hover:bottom-0 transition-all duration-500 group-hover:cursor-pointer backdrop-blur-lg bg-black bg-opacity-60' >
               <p className='flex justify-between ml-2 items-center lg:mt-8 md:mt-2 xl:mt-2 mt-8'>
               <div className='w-4/5'>
                 {/* <span className='text-[#FBBC05] font-bold text-6xl sm:text-8xl md:text-5xl lg:text-7xl xl:text-9xl '>03</span> */}
