95759574c81d1966aedbdf469193b48e2f190aa6
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-06-03 14:08:15 -0700

    changing the banner pic name

--- src/Components/Homepage/Banner/Banner.jsx
+++ src/Components/Homepage/Banner/Banner.jsx
@@ -1,11 +1,11 @@
-import slide1 from "../../../assets/images/banner.jpeg";
+import banner_pic from "../../../assets/images/banner.jpeg";
 
 const Banner = () => {
   return (
     <div
       className="relative w-screen h-[calc(100vh-4rem)] overflow-hidden bg-center bg-no-repeat bg-cover md:bg-left xl:bg-[size:150%]"
       style={{
-        backgroundImage: `url(${slide1})`,
+        backgroundImage: `url(${banner_pic})`,
       }}
     >
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 backdrop-blur-[2px] md:w-1/2 lg:justify-start md:backdrop-blur-lg">
