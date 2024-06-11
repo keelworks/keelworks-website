5ac03162145160d1e4edc902c1233827268c36ca
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-06-11 02:14:03 -0700

    using new slider images

--- src/Components/Homepage/Slider/Slider.jsx
+++ src/Components/Homepage/Slider/Slider.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import slide1 from "../../../assets/images/slide1.jpeg";
-import slide2 from "../../../assets/images/s2.jpeg";
+import slide1 from "../../../assets/images/Slides/slide1.jpg";
+import slide2 from "../../../assets/images/Slides/slide2.jpg";
 import slide3 from "../../../assets/images/card1.jpeg";
-import slide4 from "../../../assets/images/card2.jpeg";
+import slide4 from "../../../assets/images/Slides/slide4.jpg";
 import { RxDotFilled } from "react-icons/rx";
 import { useEffect } from "react";
 import Slider2 from "./SliderTexts/Slider2";
@@ -37,7 +37,9 @@ const Slider = () => {
       </div>
 
       <img
-        className="w-full h-full object-cover duration-100 relative float-right"
+        className={`w-full h-full object-cover relative float-right ${
+          currentSlideIndex === 0 ? "object-center" : ""
+        }`}
         src={slides[currentSlideIndex]}
         alt="Keelworks Slides"
       />
