24ea4803bfae84b01d1d3eab4fe56ea8a6b934fd
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-06-13 02:05:57 -0700

    adding and adjusting max-width for the whole section

--- src/Components/Homepage/Cards/Cards.jsx
+++ src/Components/Homepage/Cards/Cards.jsx
@@ -4,32 +4,37 @@ import slider3 from "../../../assets/images/card3.jpg";
 import greaterImg from "../../../assets/images/_.png";
 const Cards = () => {
   return (
-    <div className="">
-      <div className="w-full bg-grey200">
-        <div className="lg:mx-[8rem] sm:mx-[4rem] mx-[4rem]">
-          <p className="lg:text-[2.68rem] md:text-[2.25rem] sm:text-[1.875rem] text-[1.5rem] font-bold leading-relaxed lg:pt-[8rem] sm:pt-[6rem] pt-[4rem] ">
-            Poverty begets poverty. Affluence begets affluence. When we help{" "}
-            <span className="text-primary500 "> ONE </span>person to stable
-            employment, we help families, communities, and nations
-          </p>
-        </div>
-        <div className="w-full">
-          <p className="lg:text-[2.68rem] md:text-[2.25rem] sm:text-[1.875rem] text-[1.5rem] font-bold leading-relaxed lg:mx-[8rem] sm:mx-[4rem] mx-[4rem] pt-10 lg:pb-[8rem] sm:pb-[6rem] pb-[4rem]">
-            Globally,{" "}
-            <span className="text-primary500">191 million people </span> are
-            unemployed.
-            <br />
-            This is <span className="text-primary500">untapped potential.</span>
-          </p>
+    <div className="w-screen">
+      {/* ********************* Text at the Top of Cards ********************* */}
+      <div className="w-screen flex justify-center bg-grey200">
+        <div className="w-full max-w-[3000px]">
+          <div className="lg:mx-[8rem] sm:mx-[4rem] mx-[4rem]">
+            <p className="lg:text-[2.68rem] md:text-[2.25rem] sm:text-[1.875rem] text-[1.5rem] font-bold leading-relaxed lg:pt-[8rem] sm:pt-[6rem] pt-[4rem] ">
+              Poverty begets poverty. Affluence begets affluence. When we help{" "}
+              <span className="text-primary500 "> ONE </span>person to stable
+              employment, we help families, communities, and nations
+            </p>
+          </div>
+          <div className="w-full">
+            <p className="lg:text-[2.68rem] md:text-[2.25rem] sm:text-[1.875rem] text-[1.5rem] font-bold leading-relaxed lg:mx-[8rem] sm:mx-[4rem] mx-[4rem] pt-10 lg:pb-[8rem] sm:pb-[6rem] pb-[4rem]">
+              Globally,{" "}
+              <span className="text-primary500">191 million people </span> are
+              unemployed.
+              <br />
+              This is{" "}
+              <span className="text-primary500">untapped potential.</span>
+            </p>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col bg-white w-full lg:h-screen ">
-        <div className="justify-center flex pt-[3.5rem] pb-10  ">
+      {/* ********************* 3 x Cards ********************* */}
+      <div className="w-screen flex flex-col justify-center items-center bg-white lg:h-screen ">
+        <div className="w-full flex justify-center pt-[3.5rem] pb-10 ">
           <p className="text-4xl md:text-5xl lg:text-6xl font-bold ml-6 lg:ml-0 md:ml-0">
             The <span className="text-primary500 ">KeelWorks </span>Solutions
           </p>
         </div>
-        <div className="w-full grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  sm:gap-2 gap-6 lg:h-4/5">
+        <div className="w-full max-w-[3000px] grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  sm:gap-2 gap-6 lg:h-4/5">
           <div className="overflow-hidden sm:m-2 md:m-4 lg:m-4 relative group  m-2 ">
             <img src={slider1} alt="" className="w-full h-full object-cover" />
             <div className="intro h-1/6 w-full sm:p-2 md:p-2 lg:p-2 text-white absolute bottom-0 lg:group-hover:h-4/5  md:group-hover:h-3/4 group-hover:h-3/5 group-hover:bottom-0 transition-all duration-500 group-hover:cursor-pointer backdrop-blur-lg bg-black bg-opacity-60">
@@ -107,8 +112,8 @@ const Cards = () => {
         </div>
       </div>
       {/* ********************* Text at the Bottom of Cards ********************* */}
-      <div className="w-full bg-grey200">
-        <div className="mx-[4rem] md:mx-[8rem]">
+      <div className="w-full bg-grey200 flex justify-center">
+        <div className="max-w-[3000px] mx-[4rem] md:mx-[8rem]">
           <p className="text-[2rem] font-bold leading-relaxed py-[8rem] md:text-[2.68rem]">
             If you believe in our mission to get people to work, we need your
             support.
