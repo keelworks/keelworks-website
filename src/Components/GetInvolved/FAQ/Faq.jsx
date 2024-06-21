2b0a1b1ae88686f587148c8564bd2ebf0ac092b4
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-06-21 01:13:17 -0700

    creating rest of GetInvolved components

--- src/Components/GetInvolved/FAQ/Faq.jsx
+++ src/Components/GetInvolved/FAQ/Faq.jsx
@@ -1,10 +1,10 @@
-const BecomeVolunteer = () => {
+const Faq = () => {
   return (
     <div className="w-screen h-[calc(100vh-5rem)] mt-[5rem] bg-grey500 flex justify-center">
       <div className="w-full max-w-[3000px] h-full flex flex-col items-center justify-center text-8xl font-bold overflow-hidden relative">
-        Become a Volunteer
+        Frequently Asked Questions
       </div>
     </div>
   );
 };
-export default BecomeVolunteer;
+export default Faq;
--- src/Components/GetInvolved/GetInvolved.jsx
+++ src/Components/GetInvolved/GetInvolved.jsx
@@ -3,16 +3,21 @@ import Hero from "./Hero/Hero";
 import BecomeDonor from "./BecomeDonor/BecomeDonor";
 import Testimonials from "./Testimonials/Testimonials";
 import BecomeVolunteer from "./BecomeVolunteer/BecomeVolunteer";
-
+import SignUp from "./SignUp/SignUp";
+import VolunteersSay from "./VolunteersSay/VolunteersSay";
+import Faq from "./FAQ/Faq";
 // ******************************************
 
 const GetInvolved = () => {
   return (
-    <div className="w-screen bg-slate-500">
+    <div className="w-screen bg-grey300">
       <Hero />
       <BecomeDonor />
       <Testimonials />
       <BecomeVolunteer />
+      <SignUp />
+      <VolunteersSay />
+      <Faq />
     </div>
   );
 };
