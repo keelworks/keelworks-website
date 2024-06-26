0c8e29477bf231d364177adbd0dc0d816e694952
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-06-26 15:15:00 -0700

    working on responsiveness

--- src/Pages/GetInvolved/Testimonials/Testimonials.jsx
+++ src/Pages/GetInvolved/Testimonials/Testimonials.jsx
@@ -4,33 +4,25 @@ import { Link } from "react-router-dom";
 
 const Testimonials = () => {
   return (
-    <div className="w-screen h-[calc(100vh-5rem)] mt-[5rem] bg-white flex justify-center">
+    <div className="w-screen pt-[5rem] bg-white flex justify-center">
       <div className="w-full max-w-[3000px] h-full flex flex-col items-center justify-center text-8xl font-bold overflow-hidden relative">
-        <div className="flex justify-center items-center">
-          <div className="flex flex-col justify-center items-center">
+        <div className="grid gird-rows-2 md:grid-rows-1 md:grid-cols-2 mx-[4rem] md:mx-[4rem] lg:mx-[8rem]">
+          <div className="flex flex-col justify-start items-center border-[2px]">
             <div className="flex flex-col justify-center items-center">
-              <img
-                src={donor1}
-                alt="girl image"
-                className="w-2/3 border-yellow-600"
-              />
+              <img src={donor1} alt="girl image" className="w-[347px]" />
               <div></div>
             </div>
-            <p className="w-1/2 text-[1.75rem] leading-10 font-normal mt-10">
+            <p className="w-[70%] text-[1.5rem] leading-10 font-normal mt-5 mb-10 border-[2px]">
               “Donating and seeing the impact I made in real time is not rare
               with Keelworks”
             </p>
           </div>
           <div className="flex flex-col justify-center items-center">
             <div className="flex flex-col justify-center items-center">
-              <img
-                src={donor2}
-                alt="girl image"
-                className="w-2/3 border-yellow-600"
-              />
+              <img src={donor2} alt="girl image" className="w-[347px]" />
               <div></div>
             </div>
-            <p className="w-1/2 text-[1.75rem] leading-10 font-normal mt-10">
+            <p className="w-[70%] text-[1.5rem] leading-10 font-normal mt-5 mb-10 border-[2px]">
               “Getting email updates makes me feel reassured that my donations
               are going to a good cause”
             </p>
@@ -39,9 +31,9 @@ const Testimonials = () => {
         <div>
           <Link
             to="/getinvolved"
-            className="w-[300px] h-[47px] bg-primary500 rounded-full flex justify-center items-center hover:bg-primary300 transition duration-[100ms] ease-linear text-[#101828] font-semibold text-[1.31rem] mt-10"
+            className="w-[225px] h-[47px] border-primary500 border-solid border-[4px] rounded-full flex justify-center items-center hover:bg-primary300 transition duration-[100ms] ease-linear text-primary500 hover:text-fontSecondary font-semibold text-[1.31rem] mb-10"
           >
-            Become a Donor Now
+            Become a Donor
           </Link>
         </div>
       </div>
