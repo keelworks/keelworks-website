d8486501eb8c9f5f25a65f4b3babf5d33c62c4cf
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-07-02 20:45:17 -0700

    initial setup for OurSolutions page

---
+++ src/Pages/OurSolutions/KeelMate/KeelMate.jsx
@@ -0,0 +1,10 @@
+const KeelMate = () => {
+  return (
+    <div className="w-screen h-screen mt-[5rem] bg-grey500 flex justify-center">
+      <div className="w-full max-w-[3000px] h-full flex flex-col items-center justify-center text-8xl font-bold overflow-hidden relative">
+        KeelMate
+      </div>
+    </div>
+  );
+};
+export default KeelMate;
