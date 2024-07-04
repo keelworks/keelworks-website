d9fb5484936fc7ce6b474ccd5c40926bbbc353b1
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-07-03 20:20:52 -0700

    changing ImageFrame settings

--- src/Pages/GetInvolved/ImageFrame/ImageFrame.jsx
+++ src/Pages/GetInvolved/ImageFrame/ImageFrame.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const ImageFrame = ({ image, name, index }) => {
   return (
     <div className="w-[50%] m-auto md:w-[40%] h-auto relative mb-8 md:mb-16 md:mr-[6rem]">
       <div className="bg-primary500 w-full h-full absolute top-0 left-0 z-0"></div>
-      <img src={image} alt={name} className={`w-full h-auto bg-grey400 m-auto relative z-10 top-8 border-5 ${index % 2 === 0 ? 'left-10' : 'right-10'}`} />
+      <img
+        src={image}
+        alt={name}
+        className={`w-full h-auto bg-grey400 m-auto relative z-10 top-10 border-5 left-10`}
+      />
     </div>
   );
-}
+};
 
 export default ImageFrame;
--- src/Pages/GetInvolved/VolunteersSay/VolunteersSay.jsx
+++ src/Pages/GetInvolved/VolunteersSay/VolunteersSay.jsx
@@ -3,21 +3,44 @@ import Volunteer2 from "../../../assets/images/Get-Involved/Volunteer2.png";
 import ImageFrame from "../ImageFrame/ImageFrame";
 
 const volunteers = [
-  { id: 1, name: "Kathryn Tomlinson", title: 'Instructional Designer', image: Volunteer1, content: 'Working toward an MS in Instructional Design Technology, I needed experience to qualify for work in the field. The KeelWorks program gave me that experience. Working with other professionals, I created valuable portfolio components that I still use today. KeelWorks is an important part of my career growth. I use what I learned every day. KeelWorks helped me at a time when I needed direction, guidance, and experience. I gained all of these and so much more through the program. The KeelWorks motto is Learning to Live, Living to Learn: I take that to heart as I continue to learn and apply my craft.' },
-  { id: 2, name: "Kathryn Tomlinson", title: 'Instructional Designer', image: Volunteer2, content: 'Working toward an MS in Instructional Design Technology, I needed experience to qualify for work in the field. The KeelWorks program gave me that experience. Working with other professionals, I created valuable portfolio components that I still use today. KeelWorks is an important part of my career growth. I use what I learned every day. KeelWorks helped me at a time when I needed direction, guidance, and experience. I gained all of these and so much more through the program. The KeelWorks motto is Learning to Live, Living to Learn: I take that to heart as I continue to learn and apply my craft.' },
+  {
+    id: 1,
+    name: "Kathryn Tomlinson",
+    title: "Instructional Designer",
+    image: Volunteer1,
+    content:
+      "Working toward an MS in Instructional Design Technology, I needed experience to qualify for work in the field. The KeelWorks program gave me that experience. Working with other professionals, I created valuable portfolio components that I still use today. KeelWorks is an important part of my career growth. I use what I learned every day. KeelWorks helped me at a time when I needed direction, guidance, and experience. I gained all of these and so much more through the program. The KeelWorks motto is Learning to Live, Living to Learn: I take that to heart as I continue to learn and apply my craft.",
+  },
+  {
+    id: 2,
+    name: "Kathryn Tomlinson",
+    title: "Instructional Designer",
+    image: Volunteer2,
+    content:
+      "Working toward an MS in Instructional Design Technology, I needed experience to qualify for work in the field. The KeelWorks program gave me that experience. Working with other professionals, I created valuable portfolio components that I still use today. KeelWorks is an important part of my career growth. I use what I learned every day. KeelWorks helped me at a time when I needed direction, guidance, and experience. I gained all of these and so much more through the program. The KeelWorks motto is Learning to Live, Living to Learn: I take that to heart as I continue to learn and apply my craft.",
+  },
 ];
 
 const VolunteersSay = () => {
   return (
     <div className="w-screen mt-[5rem] flex justify-center flex-col">
-      <div className="w-full max-w-[3000px] flex flex-col items-center justify-center text-6xl font-bold overflow-hidden  mx-[2rem] mb-[6rem]">
+      <div className="w-full max-w-[3000px] flex flex-col items-center justify-center text-6xl font-bold overflow-hidden mx-[2rem] mb-[6rem]">
         What our Volunteers Say
       </div>
       <div className="w-full h-full flex flex-col items-center justify-center gap-[2rem]">
         {volunteers.map((volunteer, index) => {
           return (
-            <div key={index} className={`flex flex-col md:flex-row ${index % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse'} w-full md:w-[80%] mx-[2rem] md:mx-[10rem] gap-[2rem] md:gap-[7rem]`}>
-              <ImageFrame image={volunteer.image} name={volunteer.name} index={index} />
+            <div
+              key={index}
+              className={`flex flex-col md:flex-row ${
+                index % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"
+              } w-full md:w-[80%] mx-[2rem] md:mx-[10rem] gap-[2rem] md:gap-[2rem]`}
+            >
+              <ImageFrame
+                image={volunteer.image}
+                name={volunteer.name}
+                index={index}
+              />
               <div className="flex flex-col m-auto gap-[1rem] my-[2rem] w-full lg:w-[40%] p-[2rem]">
                 <h6 className="font-bold text-4xl">{volunteer.name}</h6>
                 <p className="font-bold mb-[1rem]">{volunteer.title}</p>
@@ -32,5 +55,3 @@ const VolunteersSay = () => {
 };
 
 export default VolunteersSay;
-
-
