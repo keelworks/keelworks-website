b79258fe22cdc7fac806d9142ef09e64acda2215
HeenaRecordApp <heena.dhanani@keelworks.org>
2024-07-11 11:25:17 -0500

    why keelworks

--- src/Pages/AboutUs/Hero/Hero.jsx
+++ src/Pages/AboutUs/Hero/Hero.jsx
@@ -3,9 +3,12 @@ const Hero = () => {
     <div className="w-screen h-[calc(100vh-5rem)] lg:min-h-[800px] mt-[5rem] bg-white flex justify-center items-center font-Inter">
       <div className="w-full max-w-[3000px] h-full overflow-hidden relative bg-white flex justify-center items-start">
         <div className="flex items-center justify-center flex-col xl:h-full w-full h-full mx-[4rem] lg:mx-[8rem]">
-          <div>
-            <p className="text-[3rem] leading-[3.5rem] md:text-[3.5rem] md:leading-[4rem] lg:text-[4rem] lg:leading-[4.5rem] font-bold text-[#2E2E2E] mb-[2rem]">
+          <div className="text-center ">
+            <p className="text-[40px] leading-[3.5rem] sm:text-[3rem] md:text-[3.5rem] md:leading-[4rem] lg:text-[76px] lg:leading-[4.5rem] font-bold text-[#2E2E2E] mb-[2rem]">
               About Us
+            </p>
+            <p className="mt-[40px] font-bold  text-[25px] sm:text-[30px]  lg:text-[40px] xl:text-[57px] text-[#2E2E2E]">
+            <span className="text-primary500">At KeelWorks</span>, our mission is to promote, empower, and perpetuate global economic equality through <span className="text-primary500">full inclusion</span>  at the individual level. We firmly believe  by fostering this equality, we lay the foundation for a more <span className="text-primary500"> peaceful and equitable world.</span>
             </p>
           </div>
         </div>
--- src/Pages/AboutUs/WhyKeelWorks/WhyKeelWorks.jsx
+++ src/Pages/AboutUs/WhyKeelWorks/WhyKeelWorks.jsx
@@ -1,12 +1,26 @@
 const WhyKeelWorks = () => {
   return (
-    <div className="w-screen h-[calc(100vh-5rem)] lg:min-h-[800px] mt-[5rem] bg-white flex justify-center items-center font-Inter">
-      <div className="w-full max-w-[3000px] h-full overflow-hidden relative bg-white flex justify-center items-start">
-        <div className="flex items-center justify-center flex-col xl:h-full w-full h-full mx-[4rem] lg:mx-[8rem]">
-          <div>
-            <p className="text-[3rem] leading-[3.5rem] md:text-[3.5rem] md:leading-[4rem] lg:text-[4rem] lg:leading-[4.5rem] font-bold text-[#2E2E2E] mb-[2rem]">
-              The Why of KeelWorks
-            </p>
+    <div className="w-screen h-[calc(100vh-5rem)] lg:min-h-[800px] mt-[5rem] bg-[#F5F5F9] flex   items-center font-Inter">
+      <div className="w-full max-w-[3000px] h-full overflow-hidden relative bg-[#F5F5F9] flex  items-start">
+        <div className="flex justify-start md:justify-center items-center flex-col xl:h-full w-full h-full mx-[4rem] lg:mx-[8rem]">
+          <div className="md:my-[100px] max-w-[1200px] mx-auto ">
+            <div className="sm:text-center">
+              <p className="text-[30px] leading-[3.5rem] md:text-[3rem] md:leading-[4rem] lg:text-[4rem] lg:leading-[4.5rem] font-bold text-[#2E2E2E] ">
+                The <span className="text-primary500">WHY</span>  of KeelWorks
+              </p>
+            </div>
+            <div className="text-[15px] sm:text-[18px] md:text-[20px] lg:text-[24px] text-[#646464] mt-2 lg:mt-[40px]">
+              <p>KeelWorks aims to empower individuals worldwide to reach their full potential by offering a proven recipe for success through competency development, community support, and networked intelligence.</p>
+              <p className="mt-5 lg:mt-10">Created in the state of Washington (USA) in December of 2008, KeelWorks has been supporting and empowering participants through our programs for over 15 years. At KeelWorks, we firmly believe:</p>
+              <ul className="list-disc pl-5 text-[#646464] mt-5  lg:mt-10 text-[15px] md:text-[20px] lg:text-[24px] font-bold sm:text-[18px] ">
+                <li>Unrealized human potential is everyone’s loss.</li>
+                <li>Circumstances or other people don’t define us; we define ourselves</li>
+                <li>The hunger for learning is universal</li>
+                <li>Every learner possesses unique gifts</li>
+                <li>Together, we wield limitless power</li>
+              </ul>
+              <p className="mt-5 lg:mt-10">These beliefs fuel our volunteers and donors to continually work towards our ultimate mission; a more peaceful and equitable world.</p>
+            </div>
           </div>
         </div>
       </div>
