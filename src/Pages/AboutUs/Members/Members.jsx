f861b48f5ce105623fea78055eb58c2dc7c37e73
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-07-18 17:40:09 -0700

    updating Board Members Linkedin URLs

--- src/Pages/AboutUs/Members/Members.jsx
+++ src/Pages/AboutUs/Members/Members.jsx
@@ -36,7 +36,7 @@ const volunteers = [
     content1:
       "Dana is a seasoned instructional designer based in Chicago, holding a Master’s degree and specializing in creating accessible educational content within collaborative learning environments. Her professional journey commenced with the development of math software aimed at supporting underprivileged communities in Chicago, an initiative spearheaded by her and her godmother. Additionally, Dana has played a pivotal role in a foundation that provides scholarships and grants. With a ten-year tenure at KeelWorks Foundation, she has demonstrated expertise in needs analysis, formulating learning objectives, and developing multimedia content and project management. Dana consistently seeks out challenges that enhance her competencies in project management, simulations, and a diverse array of digital media tools, underscoring her commitment to advancing educational opportunities. ",
     socialMediaLinks: {
-      linkedin: "https://www.linkedin.com/",
+      linkedin: "https://www.linkedin.com/in/danaes/",
       facebook: "https://www.facebook.com/",
       instagram: "https://www.instagram.com/",
     },
@@ -46,7 +46,7 @@ const volunteers = [
     id: 3,
     name: "Romir Patel",
     jobTitle: "Human Relations",
-    location: "New York, USA",
+    location: "Gilbert, USA",
 
     image: Volunteer3,
     content1:
@@ -62,13 +62,13 @@ const volunteers = [
     id: 4,
     name: "Jing Ma",
     jobTitle: "Instructional Designer",
-    location: "Beijing, China",
+    location: "San Francisco, USA",
 
     image: Volunteer4,
     content1:
       "Jing Ma is a native of mainland China with a decade working in the Chinese private educational technology industry. She played a leading role at a Chinese-American start-up raising capital from angel investors and venture capitalists.Jing shares the KeelWorks mission to bring everyone to the table of plenty. She has shown a desire to help the foundation in many ways. She is personally interested in exploring how finance and technology can bring disruptive innovation to education. Her entrepreneurial spirit is especially welcome to KeelWorks. Jing is an instructional designer and has supported KeelWorks instructional design for the past two years. She has been active in KeelWorks fundraising and has led a Chinese team of volunteers/interns at KeelWorks. Jing earned her Master of Science degree in Education, with a focus on Technology Leadership from California State University, East Bay, and her Bachelor of Science degree in Business Administration from Tianjin Cheng Jian University in China.",
     socialMediaLinks: {
-      linkedin: "https://www.linkedin.com/",
+      linkedin: "https://www.linkedin.com/in/jing-ma-001",
       facebook: "https://www.facebook.com/",
       instagram: "https://www.instagram.com/",
     },
@@ -84,7 +84,7 @@ const volunteers = [
     content1:
       "Ibrahima Dieng is from Senegal and currently lives in Dakar. He spent the past 15 years supporting women's rights and addressing the needs of orphan boys. He shares the KeelWorks vision of a world where everyone shares a place at the table of plenty. Having known Thomas for several years and admired his foundation, he’s honored to support this foundation's extremely important mission. 'I believe in the foundation mission and I am hopeful that by helping KeelWorks, I can help my people.'",
     socialMediaLinks: {
-      linkedin: "https://www.linkedin.com/",
+      linkedin: "https://www.linkedin.com/in/ibrahima-dieng-9abb8921/",
       facebook: "https://www.facebook.com/",
       instagram: "https://www.instagram.com/",
     },
@@ -94,13 +94,13 @@ const volunteers = [
     id: 6,
     name: "Aruna Ghildiyal",
     jobTitle: "Project Management Lead",
-    location: "New York, USA",
+    location: "San Carlos, USA",
 
     image: Volunteer6,
     content1:
       "Aruna, born and brought up in India, is an Indian American with over 17 years of experience as a leader and influencer, where she puts a focus on process improvement and compliance programs from concept to execution to ensure quality programs. She is a Corporate Lead auditor and Internal Audit Trainer for ISO 9001:2015 (Quality Management System) and ISO 14001:2015 (Environmental Management System). Aruna has a passion for communication, and an aptitude for working in cross-functional teams and multi-ethnic environments. Aruna holds a Masters Degree (M Tech) in Solid State materials, and a Master’s degree (MS) in Physics with specialization in Solid State Physics from highly reputed IITs (Indian Institute of Technology) and a Lean Six Sigma Green Belt certification from Purdue University. During her career, she has effectively managed risks, schedules, nurtured strategic customer relationships, applied mitigation strategies, and led post-mortem meetings with key stakeholders.At the Keelworks Foundation, Aruna utilizes her team player and multi-tasking capability skills to serve as the Project Management Lead. ",
     socialMediaLinks: {
-      linkedin: "https://www.linkedin.com/",
+      linkedin: "https://www.linkedin.com/in/aruna-gairola-ghildiyal-8660543/",
       facebook: "https://www.facebook.com/",
       instagram: "https://www.instagram.com/",
     },
@@ -139,7 +139,7 @@ const Members = () => {
                   <h6 className="text-1 leading-8">{volunteer.location}</h6>
                 </div>
                 {/* *************** Social Media Links ****************** */}
-                {/* <div className="flex gap-4">
+                <div className="flex gap-4">
                   <a
                     href={volunteer.socialMediaLinks.linkedin}
                     target="_blank"
@@ -148,7 +148,7 @@ const Members = () => {
                   >
                     <FaLinkedinIn />
                   </a>
-                  <a
+                  {/* <a
                     href={volunteer.socialMediaLinks.facebook}
                     target="_blank"
                     rel="noreferrer"
@@ -163,8 +163,8 @@ const Members = () => {
                     className="w-10 h-10 flex items-center justify-center border-2 border-black rounded-full"
                   >
                     <FaInstagram />
-                  </a>
-                </div> */}
+                  </a> */}
+                </div>
 
                 <p className="text-[1rem]">{volunteer.content1}</p>
               </div>
