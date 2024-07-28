65a6bcbd1f0c2128d64ceb320893e9e73571b886
shahlahassan123 <ahlan.shahla@gmail.com>
2024-07-28 12:36:04 -0500

    Implemented Success stories

--- src/Components/ImageFrame/ImageFrame.jsx
+++ src/Components/ImageFrame/ImageFrame.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 
-const ImageFrame = ({ image, name, index }) => {
+const ImageFrame = ({ image, name, index, videoLink }) => {
+  const positionClass = index % 2 === 0 ? "left-0" : videoLink ? "right-[-180px]" : "right-0";
+  const widthClass = videoLink ? "w-[calc(100%+150px)]" : "w-4/5";
+  const heightClass = videoLink ? "h-[300px]" : "h-4/5";
+
   return (
-    <div className="w-[calc(100vw-2rem)] md:w-[30%] lg:w-[35%] max-w-[500px] h-auto relative mx-[1rem] md:mx-[0] md:mb-[6rem] md:mr-[2rem]">
-      <div className="bg-primary500 w-4/5 h-4/5 absolute top-0 left-0 z-0"></div>
-      <img
-        src={image}
-        alt={name}
-        className={`w-full md:w-[300px] lg:w-full h-auto bg-grey400 m-auto relative z-10 top-5 left-5 md:top-[1.5rem] md:left-[1.5rem]`}
-      />
+    <div className="w-[calc(100vw-2rem)] md:w-[30%] lg:w-[35%] max-w-[500px] h-auto relative mx-[1rem] md:mx-[0] md:mr-[2rem]">
+      <div className={`bg-primary500 ${widthClass} ${heightClass} absolute top-0 ${positionClass} z-0`}></div>
+      {videoLink ? (
+        <iframe
+          src={videoLink}
+          className={`w-full h-[350px] md:w-[350px] md:h-[350px] lg:w-[350px] bg-grey400 m-auto relative z-10 top-5 ${index % 2 === 0 ? "left-5" : "right-5"} md:top-[1.5rem] ${index % 2 === 0 ? "left-[1.5rem]" : "right-[1.5rem]"}`}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      ) : (
+        <img
+          src={image}
+          alt={name}
+          className={`w-full md:w-[300px] lg:w-full h-auto bg-grey400 m-auto relative z-10 top-5 ${index % 2 === 0 ? "left-5" : "right-5"} md:top-[1.5rem] ${index % 2 === 0 ? "left-[1.5rem]" : "right-[1.5rem]"}`}
+        />
+      )}
     </div>
   );
 };
--- src/Pages/SuccessStories/SuccessStories.jsx
+++ src/Pages/SuccessStories/SuccessStories.jsx
@@ -1,11 +1,31 @@
+import React from "react";
 import donor1 from "../../assets/images/Get-Involved/Volunteer1.jpg";
 import donor2 from "../../assets/images/Get-Involved/Donor2.jpg";
 import { Link } from "react-router-dom";
+import ImageFrame from "../../Components/ImageFrame/ImageFrame";
+
+const videos = [
+  {
+    id: 1,
+    videoLink: "https://www.youtube.com/embed/S-PA1gjYexU?si=zNDI5zl-s208HwfJ",
+    image: donor1,
+    content1:
+      '“Donating and see the impact I made in real time is not rare with Keelworks”',
+  },
+  {
+    id: 2,
+    videoLink: "https://www.youtube.com/embed/S-PA1gjYexU?si=zNDI5zl-s208HwfJ",
+    image: donor2,
+    content1:
+      '“Getting email updates makes me feel reassured that my donations are going to a good cause”',
+  },
+
+];
 
 const SuccessStories = () => {
   return (
     <div className="w-screen pt-[5rem] bg-white flex justify-center">
-      <div className="w-full max-w-[3000px] h-full flex flex-col items-center justify-center text-8xl font-bold overflow-hidden relative">
+      <div className="w-full max-w-[3000px] h-full flex flex-col items-center justify-center text-8xl overflow-hidden relative">
         <div className="flex flex-col justify-center items-center">
           <h4 className="text-[2.5rem] leading-[3.5rem] sm:text-[3rem] md:text-[3.5rem] md:leading-[4rem] lg:text-[4.75rem] lg:leading-[5rem] font-bold text-[#2E2E2E] mt-[4rem]">
             Gallery of Video
@@ -14,9 +34,30 @@ const SuccessStories = () => {
             Case Studies
           </h4>
         </div>
-        <div className="h-screen max-w-[1600px] grid gird-rows-2 md:grid-rows-1 md:grid-cols-2 mx-[4rem] md:mx-[4rem] lg:mx-[8rem]"></div>
+        <div className="max-w-[1600px] flex flex-col md:flex-row overflow-x-scroll gap-[3rem] mx-[4rem] md:mx-[4rem] lg:mx-[8rem]">
+          {videos.map((volunteer, index) => {
+            return (
+              <div
+                key={index}
+                className={`w-full flex flex-col gap-4 justify-center mb-[4rem]`}
+              >
+                <ImageFrame
+                  videoLink={volunteer.videoLink}
+                  image={volunteer.image}
+                  index={index}
+                />
+                <div className="flex flex-col justify-center items-center gap-[1rem] w-full md:w-[75%] max-w-[500px] lg:mt-[1.5rem]">
+                  <p className="text-[1.25rem] mt-4 text-center">{volunteer.content1}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
 };
+
 export default SuccessStories;
+
+
