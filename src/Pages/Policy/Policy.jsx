085ed535169c6ad8c743975ed9a8d5d415201b09
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-10-01 19:05:52 -0700

    working on policy page

--- src/Pages/Policy/Policy.jsx
+++ src/Pages/Policy/Policy.jsx
@@ -1,11 +1,14 @@
 export const Policy = () => {
   return (
-    <div className="w-screen h-screen bg-white flex justify-center items-center">
-      <div className="w-screen max-w-[3000px] mt-[5rem] md:mt-[8rem] flex justify-center items-center">
+    <div className="w-screen h-screen bg-white flex justify-center items-start">
+      <div className="w-screen max-w-[3000px] mt-[5rem] md:mt-[8rem] flex justify-start items-center">
         <div className="mx-[1rem] md:mx-[2rem] lg:mx-[8rem]">
-          <div className="font-bold">
+          <div className="w-full flex justify-start items-start gap-[10rem]">
+            <div className="text-left">
+              <p className="text-[1rem] text-gray-600">Terms of Service</p>
+            </div>
             <div>
-              <h2 className="text-3xl md:text-[3.5rem] lg:text-5xl text-left my-[2rem]">
+              <h2 className="text-3xl md:text-[3.5rem] lg:text-5xl text-left">
                 Non-discrimination Policy Page
               </h2>
             </div>
