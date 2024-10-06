5319bdad498f602be8a9ca0ad2bffa0171222a06
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-10-05 23:18:37 -0700

    policy page

--- src/Pages/Policy/Policy.jsx
+++ src/Pages/Policy/Policy.jsx
@@ -3,7 +3,7 @@ export const Policy = () => {
     <div className="w-screen h-screen bg-white flex justify-center items-start">
       <div className="w-screen max-w-[3000px] mt-[5rem] md:mt-[8rem] flex justify-start items-center">
         <div className="mx-[1rem] md:mx-[2rem] lg:mx-[8rem]">
-          <div className="w-full flex justify-start items-start gap-[10rem]">
+          <div className="w-full flex justify-start items-start gap-[10rem] mt-[4rem]">
             <div className="text-left flex flex-col gap-2">
               <p className="text-[1.2rem] font-bold text-gray-600">
                 Terms of Service
@@ -20,6 +20,19 @@ export const Policy = () => {
               <h2 className="text-3xl md:text-[3.5rem] lg:text-5xl font-bold text-left">
                 Non-discrimination Policy Page
               </h2>
+              <p className="text-[1.2rem] text-black mt-8">
+                Effective Date:{" "}
+                <span className="text-gray-600 font-bold">July 10, 2024</span>
+              </p>
+              <p className="text-[1.2rem] text-black mt-8">
+                Purpose: KeelWorks Foundation is committed to creating an
+                inclusive environment that values diversity and promotes
+                equality for all individuals. This policy reaffirms our
+                dedication to fostering an organization and community that is
+                free from discrimination and harassment. Scope: This policy
+                applies to all KeelWorks Foundation employees, volunteers,
+                interns, contractors, vendors, and third-party partners.{" "}
+              </p>
             </div>
           </div>
         </div>
