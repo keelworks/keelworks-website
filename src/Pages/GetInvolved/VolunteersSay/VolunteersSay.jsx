eacbf26e6cfc9f2d8220e8dd76954948299a80a4
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-10-24 20:53:27 -0700

    adding new volunteers pictures and info

--- src/Pages/GetInvolved/VolunteersSay/VolunteersSay.jsx
+++ src/Pages/GetInvolved/VolunteersSay/VolunteersSay.jsx
@@ -1,5 +1,9 @@
 import Volunteer1 from "../../../assets/images/Get-Involved/Volunteer1.jpg";
 import Volunteer2 from "../../../assets/images/Get-Involved/Volunteer2.png";
+import Volunteer3 from "../../../assets/images/Get-Involved/Eric.png";
+import Volunteer4 from "../../../assets/images/Get-Involved/Madelyn.png";
+import Volunteer5 from "../../../assets/images/Get-Involved/Likith.png";
+import Volunteer6 from "../../../assets/images/Get-Involved/Thota-S.png";
 import ImageFrame from "../../../Components/ImageFrame/ImageFrame";
 
 const volunteers = [
@@ -26,6 +30,46 @@ const volunteers = [
       "I am a Senior Program Manager at Amazon. My success is the product of several career steps, all of which began with my practice in instructional design - and that started with KeelWorks. I am grateful for KeelWorks and continue to lend my full support to the foundation”.",
     signatureLine1: "With Gratitude,",
     signatureLine2: "Jonathan Stanciu",
+  },
+  {
+    id: 3,
+    name: "Eric",
+    lastName: "Halterman",
+    image: Volunteer3,
+    content1:
+      "“I have been trying to transition out of the classroom and into instructional design for a while now, and I was fortunate enough to find this internship opportunity. From the moment I joined, I was given valuable resources to help guide not only my processes, but my thinking. On top of that, Thomas has been extremely helpful in guiding me in the right direction. At first, I was just trying to teach myself tools like Articulate and Camtasia. Really, I was just wasting my time. This volunteer opportunity is giving me valuable, practical, experience in creating courses and applying ID theory. The plus side is that the mission of this organization is admirable and something that will really help people who participate in our courses. I am extremely grateful to be part of this organization, and I would encourage others to join.”",
+    signatureLine1: "With Gratitude,",
+    signatureLine2: "Eric Halterman",
+  },
+  {
+    id: 4,
+    name: "Madelyn",
+    lastName: "Banahane",
+    image: Volunteer4,
+    content1:
+      "“As someone new to the organization, I feel that KeelWorks is a great organization to start a professional journey while also contributing to a great cause and learning the ropes. The director is accessible, easy to work with and encouraging as you learn how to contribute to the organization. Getting experience, networking with a team and gaining life long colleagues are all benefits of working with KeelWorks.”",
+    signatureLine1: "With Gratitude,",
+    signatureLine2: "Madelyn Banahane",
+  },
+  {
+    id: 5,
+    name: "Likith",
+    lastName: "Prathima",
+    image: Volunteer5,
+    content1:
+      "“I've been eager to transition from teaching to instructional design for quite some time, and I was fortunate to find this internship opportunity. From the moment I joined, I received valuable resources that have guided both my processes and my thinking. Additionally, Thomas has been incredibly helpful in steering me in the right direction. Initially, I was trying to teach myself tools like Articulate and Camtasia, but I realized I was just wasting my time. This volunteer opportunity provides me with practical experience in creating courses and applying instructional design theory. On top of that, the mission of this organization is admirable and genuinely helps those who participate in our courses. I'm extremely grateful to be a part of this organization and would highly encourage others to get involved.”",
+    signatureLine1: "With Gratitude,",
+    signatureLine2: "Likith Prathima",
+  },
+  {
+    id: 6,
+    name: "Thota",
+    lastName: "Sahaja",
+    image: Volunteer6,
+    content1:
+      "“I would highly recommend Keelworks to a friend or colleague. It’s a great organization to start your professional journey, offering a supportive environment where you can learn and contribute to meaningful causes. The Keelworks team encourages growth within the role, making it a great place for gaining experience and developing long-term relationships”",
+    signatureLine1: "With Gratitude,",
+    signatureLine2: "Thota Sahaja",
   },
 ];
 
