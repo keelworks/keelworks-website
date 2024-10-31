61febce6008674cc8c17fba30b8d041f7bac4390
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-10-31 00:00:07 -0700

    first commit

---
+++ src/Pages/Blog/Blog-content.jsx
@@ -0,0 +1,15 @@
+export const Blog = () => {
+  return (
+    <div className="w-screen bg-white flex justify-center items-start">
+      <div className="w-screen max-w-[3000px] my-[5rem] md:mt-[8rem] flex justify-start items-center">
+        <div className="mx-[1rem] md:mx-[2rem] lg:mx-[8rem]">
+          <div className="h-screen">
+            <h1 className="text-[6rem]">Blog Page</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
