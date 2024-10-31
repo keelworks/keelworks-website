fa7987075f9a3c3026d931176b8f3f04ef7e8772
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-10-31 00:02:10 -0700

    Blog content setup

--- src/Pages/Blog/BlogContent.jsx
+++ src/Pages/Blog/BlogContent.jsx
@@ -1,10 +1,10 @@
-export const Blog = () => {
+export const BlogContent = () => {
   return (
     <div className="w-screen bg-white flex justify-center items-start">
       <div className="w-screen max-w-[3000px] my-[5rem] md:mt-[8rem] flex justify-start items-center">
         <div className="mx-[1rem] md:mx-[2rem] lg:mx-[8rem]">
           <div className="h-screen">
-            <h1 className="text-[6rem]">Blog Page</h1>
+            <h1 className="text-[6rem]">BlogContent</h1>
           </div>
         </div>
       </div>
@@ -12,4 +12,4 @@ export const Blog = () => {
   );
 };
 
-export default Blog;
+export default BlogContent;
