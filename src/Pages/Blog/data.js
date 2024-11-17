4b298e38a70c34fb15e6344f466e53bc8023922f
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-11-16 22:28:15 -0800

    using map to generate blog cards

--- src/Pages/Blog/data.js
+++ src/Pages/Blog/data.js
@@ -1,1 +1,27 @@
-export const newsLetters = [{ name: "test", date: "Dec 22, 2023" }];
+import newsLetter1Image from "../../assets/images/Blogs/card-1.png";
+import newsLetter2Image from "../../assets/images/Blogs/card-2.png";
+import newsLetter3Image from "../../assets/images/Blogs/card-3.png";
+
+export const newsLetters = [
+  {
+    title: "Your Support Makes A Difference",
+    date: "Dec 22, 2023",
+    description:
+      "Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit.",
+    image: newsLetter1Image,
+  },
+  {
+    title: "Your Support Makes A Difference",
+    date: "Dec 22, 2023",
+    description:
+      "Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit.",
+    image: newsLetter2Image,
+  },
+  {
+    title: "Your Support Makes A Difference",
+    date: "Dec 22, 2023",
+    description:
+      "Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit.",
+    image: newsLetter3Image,
+  },
+];
