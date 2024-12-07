9b7cc346b5b546beeceeca11d0229f965c501a23
Shabahang Saffari <shabahang.saffari@keelworks.org>
2024-12-07 00:52:06 -0800

    adding photos and data for Newsletters

--- src/Pages/Blog/data.js
+++ src/Pages/Blog/data.js
@@ -1,14 +1,21 @@
 import newsLetterImageOct24 from "../../assets/images/Blogs/Newsletter-Oct 2024-P1.jpg";
 
+import newsLetterImageMay24 from "../../assets/images/Blogs/Newsletter-May2024.jpg";
+import newsLetterImageJune24 from "../../assets/images/Blogs/Newsletter-June2024.jpg";
+import newsLetterImageAug24 from "../../assets/images/Blogs/Newsletter-Aug2024.jpg";
+import newsLetterImageSep24 from "../../assets/images/Blogs/Newsletter-Sep2024.jpg";
+import newsLetterImageOct24 from "../../assets/images/Blogs/Newsletter-Oct2024.jpg";
+import newsLetterImageNov24 from "../../assets/images/Blogs/Newsletter-Nov2024.jpg";
+
 export const newsLetters = [
   {
     id: 1003,
     title: "Newsletter",
-    date: "10/2024",
-    description: "September Newsletter",
-    image: newsLetterImageOct24,
+    date: "05/2024",
+    description: "May's Newsletter",
+    image: newsLetterImageMay24,
     pdfFile:
-      "https://drive.google.com/file/d/1tc2p4RmqGuoVvC0qONv32RENUTYYP-9E/view?usp=drive_link",
+      "https://drive.google.com/file/d/1AgovAIXWrCAZFNDvuqg7OWS176r7XFxp/view?usp=drive_link",
   },
   {
     id: 1002,
